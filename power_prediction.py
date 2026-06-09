8b37dc91417d8c0c2a79ec8ded853f001d13775c
Nick-Kevin <nicblou23@gmail.com>
2026-06-10 02:23:32 +0400

    update : sub-metering label

--- power_prediction.py
+++ power_prediction.py
@@ -44,7 +44,7 @@ def main ():
     voltage = st.number_input("Voltage (in volt)", value=233.63)
     intensity = st.number_input("Global intensity (in ampere)", value=23)
     sub_metering_1 = st.number_input(
-        "Sub metering 1: energy sub-metering No. 1"
+        "Sub metering 1:\nEnergy sub-metering No. 1"
         "(in watt-hour of active energy)." \
         "It corresponds to the kitchen, containing mainly a dishwasher," \
         "an oven and a microwave (hot plates are not electric but gas powered)"
